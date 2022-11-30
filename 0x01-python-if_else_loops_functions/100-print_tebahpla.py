36ce8e7bb85c7876c73e76d2751765f4974281db
simamkele21 <simamkelejanuary@gmail.com>
2022-11-30 12:22:19 +0200

    .

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 i = 122
 while i >= 97:
-    diff = 0 
+    diff = 0
     if i % 2 != 0:
-        i = i + 32
+        i = i - 32
         diff = 1
     print("{:s}".format(chr(i)), end="")
     if diff == 1:
